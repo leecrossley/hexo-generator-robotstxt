31b26ca6bc1010cc87725d04909e42a7167338c6
WanielDeiss <yoda@webjedi.de>
2015-05-15 15:41:59 +0200

    Bugfix and more functions

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ hexo.extend.generator.register('robotstxt', function(locals){
 		path: 'robots.txt',
 		data: function(){
 			var cfg = hexo.config.robotstxt;
-			var body = "User-agent: " + cfg.useragent + "\n";
-			cfg.disallow.forEach(function(entry) {
-				body += "Disallow: " + entry + "\n";
-			});
+			console.log(cfg);
+			var body = "User-agent: " + (cfg.useragent ? cfg.useragent : '*' )  + "\n";
+
+			if( cfg.disallow ) {
+				cfg.disallow.forEach(function(entry) {
+					body += "Disallow: " + entry + "\n";
+				});
+			}
+
+			if( cfg.allow ) {
+				cfg.allow.forEach(function(entry) {
+					body += "Allow: " + entry + "\n";
+				});
+			}
+
+			if( cfg.sitemap ) {
+				body += "Sitemap: " + cfg.sitemap + "\n";
+			}
+
 			return body;
 		}
 	};
